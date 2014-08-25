353ccae4ff03191be5fcc4bda1d57c5319f378ab
Michael Lavers <kolanos@gmail.com>
2014-08-24 20:50:09 -0700

    Up version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='namesilo',
-    version='0.1.1',
+    version='0.1.2',
     url='http://github.com/kolanos/namesilo',
     license='MIT',
     author='Michael Lavers',

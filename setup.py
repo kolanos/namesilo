a699a4ad46f2e2d92d3f665451d7c6438879e4aa
Teran McKinney <sega01@go-beyond.org>
2018-02-08 02:17:19 +0000

    0.1.5: Fix update_registered_nameserver()
    Looks like NameSilo has removed the updateRegisteredNameServer
    endpoint, so we have to use the modifyRegisteredNameServer.
    
    Before this commit, ordering was causing updateRegisteredNameServer
    to be used.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='namesilo',
-    version='0.1.4',
+    version='0.1.5',
     url='http://github.com/kolanos/namesilo',
     license='MIT',
     author='Michael Lavers',
